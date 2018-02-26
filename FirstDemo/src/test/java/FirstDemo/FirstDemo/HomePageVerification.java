93d1c59656152c194e50b75cab13fb5b1b51cbc9
raghvendra <raghvendra.qa@gmail.com>
2018-02-26 23:37:22 +0530

    First Demo First Chekin

---
+++ FirstDemo/src/test/java/FirstDemo/FirstDemo/HomePageVerification.java
@@ -0,0 +1,12 @@
+package FirstDemo.FirstDemo;
+import org.testng.annotations.Test;
+public class HomePageVerification {
+	
+	
+	@Test
+	public void verifyGooglePageTittle() {
+		System.out.println("We are in verifyGooglePageTittle");
+	}
+	
+	
+}
